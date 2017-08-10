44ba3aaa189b0ca3ac721460878db6767dfd9e67
ognyanstanoev <ognyanstanoev@gmail.com>
2017-08-10 20:57:08 +0300

    Added number of products on about us and number of testimonials on contact us

--- src/app/core/about.component.ts
+++ src/app/core/about.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import {ProductService} from '../products/product.service'
 
 @Component({
     selector:'',
     template:`
     <h1>About us</h1>
     <h3>This is about us page and I am so glad you are looking at it</h3>
+    <br>
+    <h4 *ngIf="productsNumber">We have <strong><i>{{productsNumber}}</i></strong> products on our site from which you can choose!</h4>
     `
 })
 
 export class AboutUsComponent {
+    productsNumber
 
+    constructor(private productService: ProductService) {}
+
+    ngOnInit(){
+     this.productService.getAll().subscribe(res => {
+           this.productsNumber = res.length
+        } )
+
+    }
 }
--- src/app/core/contacts.component.ts
+++ src/app/core/contacts.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core'
+import {TestimonialsService} from '../testimonials/testimonial.service'
 
 @Component({
   selector: 'contacts',
   templateUrl: './contacts.component.html'
 })
-export class ContactsPageComponent {}
+export class ContactsPageComponent {
+    testimonialsNumber
+
+    constructor(private testimonialsService: TestimonialsService) {}
+
+    ngOnInit(){
+      //TODO: Change getDataForApproval with getDataApproved when we have approved testimonials by admin
+     this.testimonialsService.getDataForApproval().subscribe(res => {
+           this.testimonialsNumber = res.length
+        } )
+  }
+}
